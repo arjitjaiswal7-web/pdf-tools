8679b1527f916af5cef28048813b018d105b0da8
Arjit Jaiswal <arjitjaiswal@Arjits-MacBook-Air.local>
2026-02-21 02:42:48 +0530

    Restructure route to /pdf-tools/merge-pdf

--- app/merge/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MergePage() {
-  return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Merge PDF Tool</h1>
-      <p>This is the merge tool page.</p>
-    </main>
-  );
-}
